c0c47a371a2d7520943fef1dbc5ef8f5655fa28c
Danchoys <danchoys@mail.ru>
2014-03-24 11:22:53 +0400

    -Added comments to the delegate methods.

--- DKCloudCoreDataController/DKCloudCoreDataController.h
+++ DKCloudCoreDataController/DKCloudCoreDataController.h
@@ -13,16 +13,35 @@
 
 @protocol DKCloudCoreDataControllerDelegate <NSObject>
 
+@required
+
+/**
+ This method is used to merge data from one context into another. You typically have to iterate through
+ data inside the fromContext and create same objects inside the toContext. You shouldn't worry much about possible
+ duplicates: the controller will perform duplicate resolving itself after this method is executed.
+ */
 - (void)cloudCoreDataController:(DKCloudCoreDataController *)controller
                    mergeContext:(NSManagedObjectContext *)fromContext
                     intoContext:(NSManagedObjectContext *)toContext;
 
+/**
+ From this method you need to return the attribute name which uniquely identifies an object of the given entity. If
+ this entity does not have unique attributes, you need to return nil.
+ */
 - (NSString *)cloudCoreDataController:(DKCloudCoreDataController *)controller uniqueAttributeNameForEntityWithName:(NSString *)enityName;
 
+/**
+ From this method you need to return the attribute name which allows for comparing objects of the given entity. The type
+ of the attribute has to be one of this: NSString, NSNumber, NSDate.
+ */
 - (NSString *)cloudCoreDataController:(DKCloudCoreDataController *)controller comparisonAttributeNameForEntityWithName:(NSString *)enityName;
 
 @optional
 
+/**
+ Use this method to notify the user that his data from the NoAccount store has been merged into the Cloud store and that
+ it is no longer available when he logs off from his current iCloud account.
+ */
 - (NSString *)cloudCoreDataControllerDidMergeNoAccountStoreIntoCloudStore:(DKCloudCoreDataController *)controller;
 
 @end
